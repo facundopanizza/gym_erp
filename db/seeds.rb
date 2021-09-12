f4305c9baf23849d09ff23f33fba1188a6a1ba31
Facundo Panizza <facundo@panizza.dev>
2021-09-12 19:52:43 -0300

    Agrego seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+teacher = Teacher.create(first_name: "Oscar", last_name: "Oscar", dni: "12312312", phone_number: "1231231231", birthday: "1990-01-01")
+
+user = User.create(email: "oscar@gmail.com", password: "123456", teacher: teacher)
+
