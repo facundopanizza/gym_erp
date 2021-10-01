33293cabc7a133938e46a4ec4e6abda44204bed5
Facundo Panizza <facundo@panizza.dev>
2021-09-30 22:00:49 -0300

    Arreglo activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ApplicationRecord
   has_one :price, dependent: :destroy
   accepts_nested_attributes_for :price, allow_destroy: true
 
-  validates :name, presence: true, numericality: true
+  validates :name, presence: true
   validates :price, presence: true, numericality: true
 
   def to_label
